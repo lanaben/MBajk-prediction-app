8aee9329c9903c7db17491f818ed61a345fd15cd
Lana Benedicic <lana.benedicic@gmail.com>
2024-04-18 20:44:21 +0200

    Fetch weather fix.

--- src/data/fetch_weather_data.py
+++ src/data/fetch_weather_data.py
@@ -6,10 +6,9 @@ from datetime import datetime
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR, '..', '..', 'data')
-raw_data_dir = os.path.join(DATA_DIR, "raw")
-weather_data_dir = os.path.join(raw_data_dir, "weather")
+bike_data_dir = os.path.join(DATA_DIR, "raw", "bikes")
+weather_data_dir = os.path.join(DATA_DIR, "raw", "weather")
 os.makedirs(weather_data_dir, exist_ok=True)
-
 
 def normalize_station_name(station_name):
     normalized_name = unicodedata.normalize('NFKD', station_name).encode('ASCII', 'ignore').decode('utf-8')
@@ -24,16 +23,13 @@ def fetch_weather_data(latitude, longitude):
         return None
 
 def main():
-    # Load station data from previously saved files
-    station_files = [f for f in os.listdir(raw_data_dir) if f.endswith('_raw_station_data.json')]
+    station_files = [f for f in os.listdir(bike_data_dir) if f.endswith('_raw_station_data.json')]
     for filename in station_files:
-        file_path = os.path.join(raw_data_dir, filename)
+        file_path = os.path.join(bike_data_dir, filename)
         with open(file_path, 'r') as file:
             data = json.load(file)
 
-        # Check if data is a list and access the first element
         station = data[0] if isinstance(data, list) else data
-
         latitude = station['position']['lat']
         longitude = station['position']['lng']
         weather_data = fetch_weather_data(latitude, longitude)
